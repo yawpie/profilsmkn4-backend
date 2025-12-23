4deb402f85acf36b122e434a0ae17fcd726c7c72
Muhammad Rafi <iamyawpie@protonmail.com>
2025-12-23 16:40:20 +0800

    fix: change content and description field types to TEXT in multiple models

---
+++ prisma/migrations/20251223080403_added_text_type_untuk_pengumuman_supaya_bisa_panjang/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "announcements" ALTER COLUMN "content" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "articles" ALTER COLUMN "content" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "extracurriculars" ALTER COLUMN "description" SET DATA TYPE TEXT;
---
+++ prisma/migrations/20251223082346_added_text_type_untuk_model_supaya_bisa_panjang_part2/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "facilities" ALTER COLUMN "description" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "majors" ALTER COLUMN "description" SET DATA TYPE TEXT;
