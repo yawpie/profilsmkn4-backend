906f320cba3aafeaffd3111de748b799899a2bc5
Muhammad Rafi <iamyawpie@protonmail.com>
2026-01-29 15:34:43 +0800

    fix: remove tagsId column from articles model and update migration script

---
+++ prisma/migrations/20260129073405_hilangkan_tagsid_di_articles/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tagsId` on the `articles` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "articles" DROP COLUMN "tagsId";
